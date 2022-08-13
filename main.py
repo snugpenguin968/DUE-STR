9b75eaf6113546b31fa8de8a0739309731f34636
John Tian <johntian2014@gmail.com>
2022-08-12 20:15:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ STR_SUMO.py, RouteController.py, Util.py, test.net.xml, test.rou.xml, myconfig.s
 print("what")
 from core.STR_SUMO import StrSumo
 from core.STR_SUMO_Meta_collector_plus import StrSumo_Meta_collector_plus
-
-from core.STR_SUMO_overall_performance import StrSumo_Overall
 import os
 import sys
 import copy
@@ -21,9 +19,10 @@ import core.Run_id
 from controller.New_Tom_Reader_Controller import NewPolicy_Reader
 from controller.STRBE_Controller import NewPolicy_Reader_STRBE
 from controller.STRBE_Collector import STRBE_Collector
-from controller.Reader_Controller import Reader
 from Route_setup.Make_Trips_file import *
-
+from controller.ShortestTime import ShortestTimeController
+from controller.FloydWarshall import FWController
+from controller.SelflessController import SelflessPolicy_Reader
 from core.target_vehicles_generation_protocols import *
 #from history.Historical_Prediction import *
 from datetime import datetime
@@ -38,7 +37,7 @@ else:
 from sumolib import checkBinary
 import traci
 
-    
+
 # net_map = root.xpath('./input/net-file/@value')[0]
 Round_name_base ="Eric"#"analyze"#'for_paper'##"analyze-two-"#'dua_iterate_analysis'#'for_paper'##"tests"#
 sufix = 0
@@ -47,15 +46,15 @@ round_max = 1
 
 even = False # if true then we will send control_v number of vehicles down each start edge
 
-control_v_min = 300 #number of controlled cars
-control_v_max = 500
-uncontrol_v = 0 
+control_v_min = 500 #number of controlled cars
+control_v_max = 1000
+uncontrol_v = 50
 start_edge = "-2"#-1 # for random generation
 end_edge = "-2"# for random generation
 pattern_var = 3
 refine = True # use duaiterate.py
 #already_routed = True
-Using_reading = True# if false then use target_vehicles_generation and if false with already routed then back to 
+Using_reading = True# if false then use target_vehicles_generation and if false with already routed then back to
 
 #how the original code work.
 trips = {}
@@ -71,7 +70,8 @@ edge_dependencies = {"597602756#0":["-597602756#0"],"-260998164#3":["260998164#0
 
 trip_file_name = "static_trip.rou.xml"
 file_name = "str_sumo.rou.xml" #should be from duarouter
-file_name2 = "str_sumo_099.rou.xml"#"static2_099.rou.xml"#should be from duaIterate 
+
+file_name2 = "str_sumo_099.rou.xml"#"static2_099.rou.xml"#should be from duaIterate
 file_name3 = "One-shot.rou.xml"
 num_iterations = 100
 
@@ -98,7 +98,7 @@ def get_controlled_vehicles(route_filename, connection_info, \
     if pattern_var != 4:
         start_and_end_edges = []
     generator = target_vehicles_generator(connection_info.net_filename)
-    
+
     # list of target vehicles is returned by generate_vehicles
     vehicle_list = generator.generate_vehicles(num_controlled_vehicles, num_uncontrolled_vehicles, \
         pattern, route_filename, connection_info.net_filename,start_edge,end_edge)#, \
@@ -106,7 +106,7 @@ def get_controlled_vehicles(route_filename, connection_info, \
 
     for vehicle in vehicle_list:
         vehicle_dict[str(vehicle.vehicle_id)] = vehicle
-    
+
     return vehicle_dict
 
 def test_dijkstra_policy(vehicles):
@@ -115,15 +115,11 @@ def test_dijkstra_policy(vehicles):
     run_simulation(scheduler, vehicles)
 
 
-def test_new_policy_Meta_Plus(vehicles,fiuela): #reader using meta SUMO 
+def test_new_policy_Meta_Plus(vehicles,fiuela): #reader using meta SUMO
     print("Testing New Algorithm meta data collection Reader Route Controller")
     scheduler = NewPolicy_Reader(init_connection_info,fiuela,Round_name)#going to add file name as another argument
     run_simulation_Meta_Plus(scheduler, vehicles)
     del scheduler
-    
-def test_Reader(vehicles,fiuela): # simplified reader
-    scheduler = Reader(init_connection_info,fiuela,Round_name)#going to add file name as another argument
-    run_simulation_Meta_RL(scheduler, vehicles,net_file, False)
 
 def test_new_policy_STRBE_Meta_Plus(vehicles,time_difference,predictors):# STRBE Controller
     print("Testing New Algorithm meta data collection Reader Route Controller")
@@ -134,6 +130,25 @@ def test_new_policy_STRBE_Meta_Plus(vehicles,time_difference,predictors):# STRBE
 def test_new_policy_STRBE_collector_Meta_Plus(vehicles,time_difference): # STRBE collector
     print("Testing New Algorithm meta data collection Reader Route Controller")
     scheduler = STRBE_Collector(init_connection_info,file_name,file_name2,vehicles,time_difference)#going to add file name as another argument
+    run_simulation_Meta_Plus(scheduler, vehicles)
+    del scheduler
+
+
+def ShortestTime(vehicles,file):
+    print('Testing Shortest Time Controller')
+    scheduler = ShortestTimeController(init_connection_info, file, Round_name)
+    run_simulation_Meta_Plus(scheduler, vehicles)
+    del scheduler
+
+def test_FloydWarshall(vehicles,file):
+    print('Testing Floyd Warshall Controller')
+    scheduler = FWController(init_connection_info, file, Round_name)
+    run_simulation_Meta_Plus(scheduler, vehicles)
+    del scheduler
+
+def test_selfless_policy(vehicles, file):
+    print('Testing selfless algorithm')
+    scheduler = SelflessPolicy_Reader(init_connection_info, file, Round_name)
     run_simulation_Meta_Plus(scheduler, vehicles)
     del scheduler
 
@@ -156,33 +171,12 @@ def run_simulation(scheduler, vehicles):
             ".format(step,str(total_time/end_number),max_time,str(end_number),str(fin)))
         if deadlines_missed == 0:
             print(str(deadlines_missed) + ' deadlines missed.' + " average time over deadline:"+ str(0))
-        else: 
+        else:
             print(str(deadlines_missed) + ' deadlines missed.' + " average time over deadline:"+str(deadline_ogretime/deadlines_missed+1))
 
     print("TRACI CLOSEING")
     traci.close()
 
-
-def run_simulation_Meta_RL(scheduler, vehicles,net_file,recording): # for real time STRRT
-    #net file is used to access the realtime data for a specific map, only implemented for this scheme atm
-    simulation = StrSumo_Overall(scheduler, init_connection_info, vehicles,Round_name_base+str(sufix),net_file,recording) 
-
-    traci.start([sumo_binary, "-c", "./configurations/myconfig.sumocfg", \
-                 "--tripinfo-output", "./configurations/trips.trips.xml", \
-                 "--fcd-output", "./configurations/testTrace.xml","--quit-on-end"])
-
-    step, total_time, end_number, deadlines_missed,deadline_ogretime,max_time,fin = simulation.run()
-
-    if end_number != 0:
-        print("Total timespan: {}, Average timespan: {}, Max Travel Time {}, total vehicle number: {}, Total Vehicles finished: {}\
-            ".format(step,str(total_time/end_number),max_time,str(end_number),str(fin)))
-        if deadlines_missed == 0:
-            print(str(deadlines_missed) + ' deadlines missed.' + " average time over deadline:"+ str(0))
-        else: 
-            print(str(deadlines_missed) + ' deadlines missed.' + " average time over deadline:"+str(deadline_ogretime/deadlines_missed+1))
-
-    print("TRACI CLOSEING")
-    traci.close()
 
 def run_simulation_Meta_Plus(scheduler, vehicles): # used in STRBE non-real time
 
@@ -199,7 +193,7 @@ def run_simulation_Meta_Plus(scheduler, vehicles): # used in STRBE non-real time
             ".format(step,str(total_time/end_number),max_time,str(end_number),str(fin)))
         if deadlines_missed == 0:
             print(str(deadlines_missed) + ' deadlines missed.' + " average time over deadline:"+ str(0))
-        else: 
+        else:
             print(str(deadlines_missed) + ' deadlines missed.' + " average time over deadline:"+str(deadline_ogretime/deadlines_missed+1))
 
     print("TRACI CLOSEING")
@@ -216,12 +210,12 @@ if __name__ == "__main__":
     net_file = net_file_attr['value'].nodeValue
 
     mapsqm = str(net_file).find("/maps/")
-    if mapsqm != -1: 
+    if mapsqm != -1:
         slash_pos = net_file.find("/",mapsqm+1)
         net_file = net_file[slash_pos+1:]
         #I want to move all the map files into configurations/maps but the config file wants me to specifify /maps/{map_name}
         #I would just remove the /maps/ from below but I want to get just the file name meaning I would have to do this anyways
-    
+
 
     init_connection_info = ConnectionInfo("./configurations/maps/"+net_file)
     print("net_file:",net_file)
@@ -229,13 +223,13 @@ if __name__ == "__main__":
     route_file_attr = route_file_node[0].attributes
     route_file = "./configurations/maps/"+route_file_attr['value'].nodeValue
 
-    
+
     #global Round_name
     #this is where a loop may occur in case of increaseing number of vehicles
-    
+
     predictors = Time_predictors(net_file)
     sumo_binary = checkBinary('sumo')#'sumo-gui' or # 'sumo'
-        
+
     for increment in range(round_min,round_max): #,control_v,
         sufix = increment
         core.Run_id.Round_sufix =Round_name_base+str(sufix)
@@ -249,44 +243,46 @@ if __name__ == "__main__":
         print("route file = ",route_file)
 
 
-        if Using_reading == False:  
+        if Using_reading == False:
             # using the code in a more traditional sense to how it was written by guangli and pavan.
             #that is to say that the routes are updated after a vehicle gets on a new edge and is met with a new
             #edge node pair
-           
+
             num_v =control_v_max#= random.randint(control_v/2,control_v)
             vehicles = get_controlled_vehicles(route_file, init_connection_info,num_v, uncontrol_v,3)
         else:
-            
-            
+
+
             if os.path.isdir("./configurations/rounds/"+Round_name):
-                
+
                 vehicles = read_vehicle_file(trip_file_name,Round_name,net_file)
             else:
                 num_v = random.randint(control_v_min, control_v_max)#control_v
                 vehicles = Create_trips(start_edges,end_edges,edge_dependencies,num_v,uncontrol_v,Round_name,even,net_file,refine,num_iterations)
         core.Run_id.Controller_version = "N/A"
-            
-        
+
+
 
         tom_vehicles = copy.deepcopy(vehicles)
         #iterator = 0
         tom_vehicles2 = copy.deepcopy(tom_vehicles)
 
-     
+
 
         #DUE-STR
         # core.Run_id.run_id = "STRBE_DUE-STR"
         # test_new_policy_STRBE_Meta_Plus(tom_vehicles2,100000,predictors)
         # tom_vehicles2 = copy.deepcopy(tom_vehicles)
 
-        #for testing dijkstras 
-        # core.Run_id.run_id = "str_sumo.rou.xml"
-        # test_new_policy_Meta_Plus(tom_vehicles2,file_name)
-        # tom_vehicles2 = copy.deepcopy(tom_vehicles)
+        #for testing dijkstras
+        core.Run_id.run_id = "str_sumo.rou.xml"
 
-        core.Run_id.run_id = "Dijkstras-Reader2"
-        test_Reader(tom_vehicles2,file_name)
+        #test_new_policy_Meta_Plus(tom_vehicles2,file_name)
+        #ShortestTime(tom_vehicles2,file_name)
+        #test_dijkstra_policy(vehicles)
+        #test_FloydWarshall(vehicles,file_name)
+        test_selfless_policy(vehicles,file_name)
+
         tom_vehicles2 = copy.deepcopy(tom_vehicles)
 
 
@@ -297,7 +293,7 @@ if __name__ == "__main__":
         # core.Run_id.run_id = "STRBE_Collector"
         # test_new_policy_STRBE_collector_Meta_Plus(tom_vehicles2,file_name2)
         # tom_vehicles2 = copy.deepcopy(tom_vehicles)
-        #test_dijkstra_policy(tom_vehicles2)
+
     if os.path.isdir("./0"):
         for x in range(0,num_iterations):
             shutil.rmtree('./'+str(x))
@@ -305,4 +301,4 @@ if __name__ == "__main__":
             #if there are more duaiterate folders than num_iterations then it is a problem.
 
 
-    
+  
